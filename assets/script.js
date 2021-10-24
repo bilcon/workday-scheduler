30d0e0094d1c83cc3f717ee847d380623bb51b95
Bill Morris <bilcon@swbell.net>
2021-10-24 08:17:53 -0500

    added hours

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,10 @@
 var currentDayText = moment().format("[Today is ]MMMM DD, YYYY");
 $("#currentDay").text(currentDayText);
+
+var times = { "9 AM": "", "10 AM": "", "11 am": "", "12 PM": "", "1 PM": "", "2 PM": "", "3 PM": "", "4 PM": "", "5 PM": "", };
+
+
+
+$(document).ready(function () {
+
+});
